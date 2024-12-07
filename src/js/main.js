bcfae0ffb9eb2daac4237bebaa74238634b6b68a
Даниил <danilka.volkov.02@mail.ru>
2024-12-07 03:30:55 +0300

    Инициализация

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+  /**
+   * В load добавляем скрипты, которые следует грузить позже.
+   */
+  window.addEventListener('load', () => {
+
+  })
+})
